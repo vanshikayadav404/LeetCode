d94dbed33cf5ff8a631e7ea3682d9d09b2c11f31
Vanshika Yadav <vanshivikram2006@gmail.com>
2026-07-05 15:59:33 +0530

    Time: 1 ms (99.92%), Space: 43.4 MB (19.83%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.java
@@ -0,0 +1,15 @@
+class Solution {
+
+    public char findTheDifference(String s, String t) {
+
+        int sum = 0;
+
+        for(char ch : t.toCharArray())
+            sum += ch;
+
+        for(char ch : s.toCharArray())
+            sum -= ch;
+
+        return (char) sum;
+    }
+}
