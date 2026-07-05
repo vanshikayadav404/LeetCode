60cf4f9ac424d2b2ed22c82a88850f66afaf9b98
Vanshika Yadav <vanshivikram2006@gmail.com>
2026-07-05 15:40:03 +0530

    Time: 0 ms (100%), Space: 42.9 MB (49.94%) - LeetHub

---
+++ 0500-keyboard-row/0500-keyboard-row.java
@@ -0,0 +1,40 @@
+class Solution {
+
+    public String[] findWords(String[] words) {
+
+        String row1 = "qwertyuiop";
+        String row2 = "asdfghjkl";
+        String row3 = "zxcvbnm";
+
+        List<String> ans = new ArrayList<>();
+
+        for (String word : words) {
+
+            String lower = word.toLowerCase();
+
+            String row;
+
+            if (row1.indexOf(lower.charAt(0)) != -1)
+                row = row1;
+            else if (row2.indexOf(lower.charAt(0)) != -1)
+                row = row2;
+            else
+                row = row3;
+
+            boolean valid = true;
+
+            for (int i = 1; i < lower.length(); i++) {
+
+                if (row.indexOf(lower.charAt(i)) == -1) {
+                    valid = false;
+                    break;
+                }
+            }
+
+            if (valid)
+                ans.add(word);
+        }
+
+        return ans.toArray(new String[0]);
+    }
+}
