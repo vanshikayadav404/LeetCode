ccaae8d00c7f1b418ac0789bc944aba0329ae511
Vanshika Yadav <vanshivikram2006@gmail.com>
2026-07-15 23:12:49 +0530

    Time: 3 ms (87.57%), Space: 47.1 MB (97.79%) - LeetHub

---
+++ 0135-candy/0135-candy.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int candy(int[] ratings) {
+        int n = ratings.length;
+
+        int[] candies = new int[n];
+
+        // Give every child 1 candy initially
+        Arrays.fill(candies, 1);
+
+        // Left to Right
+        for (int i = 1; i < n; i++) {
+            if (ratings[i] > ratings[i - 1]) {
+                candies[i] = candies[i - 1] + 1;
+            }
+        }
+
+        // Right to Left
+        for (int i = n - 2; i >= 0; i--) {
+            if (ratings[i] > ratings[i + 1]) {
+                candies[i] = Math.max(candies[i], candies[i + 1] + 1);
+            }
+        }
+
+        // Calculate answer
+        int total = 0;
+        for (int candy : candies) {
+            total += candy;
+        }
+
+        return total;
+    }
+}
